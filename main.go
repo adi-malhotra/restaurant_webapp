7dbd01bc2765a84c4051b01070940f58920cec4a
Aditya Malhotra <adi.malhotra02@gmail.com>
2019-03-23 01:21:33 +0530

    adding templates and database connectivity

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type person struct {
+	Name     string
+	DBStatus bool
+}
+
 func main() {
+
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+	db, _ := sql.Open("sqlite3", "dev.db")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		p := person{Name: "Aditya"}
+		name := r.FormValue("name")
+		if name != "" {
+			p.Name = name
+		}
+		p.DBStatus = db.Ping() == nil
+		err := templates.ExecuteTemplate(w, "index.html", p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
