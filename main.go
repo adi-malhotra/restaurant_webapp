c0053989a588c19fb59bcef7b42596d580f43254
Aditya Malhotra <adi.malhotra02@gmail.com>
2019-03-24 00:59:00 +0530

    added dummy values in table on submit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -12,6 +13,14 @@ import (
 type person struct {
 	Name     string
 	DBStatus bool
+}
+
+type Results struct {
+	RestaurantName string
+	Location       string
+	Cuisines       string
+	UserRating     float32
+	AvgCost        int
 }
 
 func main() {
@@ -26,6 +35,20 @@ func main() {
 		}
 		p.DBStatus = db.Ping() == nil
 		err := templates.ExecuteTemplate(w, "index.html", p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []Results{
+			Results{"Levels", "Hauz Khas Village", "Italian, North Indian, Mexican", 4.2, 1200},
+			Results{"Local", "Connaught Place", "Buffet", 3.9, 1500},
+			Results{"Diggin", "Chankyapuri", "Mexican, Greek, Italian, Indian", 4.4, 2000},
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		encoder := json.NewEncoder(w)
+		err := encoder.Encode(results)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
